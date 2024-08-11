8813fa7b8da79cc4a8f2b9eac539b09af41cf59c
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-08-11 17:32:04 +0300

    create models

--- lexicon/settings.py
+++ lexicon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "medlexicon",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject.urls'
+ROOT_URLCONF = 'lexicon.urls'
 
 TEMPLATES = [
     {
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'medlexicon.Worker'
--- medlexicon/models.py
+++ medlexicon/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Worker(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+
+class WordFormat(models.Model):
+    format_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.format_name
+
+
+class Word(models.Model):
+    text = models.CharField(max_length=200)
+    translation_uk = models.CharField(max_length=200)  # Перевод на украинский
+    translation_pl = models.CharField(max_length=200)  # Перевод на польский
+    date_added = models.DateTimeField(auto_now_add=True)
+    word_format = models.ForeignKey(WordFormat, on_delete=models.CASCADE)
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text

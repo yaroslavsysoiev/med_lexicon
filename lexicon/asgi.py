e8f6ea520361db8bdfc0d532def3ca1dc946d04e
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-08-11 17:21:35 +0300

    refactor: Refactor the project name to lexicon

--- lexicon/asgi.py
+++ lexicon/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for med_lexicon project.
+ASGI config for lexicon project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'med_lexicon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lexicon.settings')
 
 application = get_asgi_application()

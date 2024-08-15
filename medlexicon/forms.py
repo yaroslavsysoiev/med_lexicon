7f4a0ccb8eed362b240986065b2ea6e131de4b22
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-08-15 14:44:58 +0300

    Create WorkerPhoneUpdateForm

--- medlexicon/forms.py
+++ medlexicon/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from .models import Word, Category, WordFormat
+from .models import Word, Category, WordFormat, Worker
 
 
 class WordForm(forms.ModelForm):
@@ -53,8 +53,23 @@ class WorkerCreationForm(UserCreationForm):
             raise ValidationError("First name must be at least 2 characters long")
         return first_name
 
+    def validate_word_length(text):
+        if len(text) < 2:
+            raise ValidationError("The word must be at least 2 characters long")
+        return text
 
-def validate_word_length(text):
-    if len(text) < 2:
-        raise ValidationError("The word must be at least 2 characters long")
-    return text
+
+class WorkerPhoneUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = ["phone_number"]
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get("phone_number")
+
+        if not phone_number.isdigit():
+            raise forms.ValidationError("Phone number must contain only digits.")
+        if len(phone_number) < 7 or len(phone_number) > 15:
+            raise forms.ValidationError("Phone number must be between 7 and 15 digits.")
+
+        return phone_number

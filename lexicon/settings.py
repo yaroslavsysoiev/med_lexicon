35e739a4d4021e8fc017ee796a42ab12d859083a
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-08-17 16:08:22 +0300

    config: update static files configuration

--- lexicon/settings.py
+++ lexicon/settings.py
@@ -147,9 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'medlexicon.Worker'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
